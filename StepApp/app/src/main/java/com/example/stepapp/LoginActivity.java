2dec2be12c5481dc111bc07c041669d67dc6a5cf
nikomyri <nikolaos.myrillas2021@yahoo.com>
2021-11-30 14:56:24 +0100

    happyfeetfixedalmost

--- StepApp/app/src/main/java/com/example/stepapp/LoginActivity.java
+++ StepApp/app/src/main/java/com/example/stepapp/LoginActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
@@ -19,6 +20,20 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View view) {
                 startActivity(new Intent(LoginActivity.this,RegisterActivity.class));
 
+            }
+        });
+        // add login functionality
+        Button registerButton = findViewById(R.id.register_btn);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: login new user
+
+                // step 1: find email
+                // step 2: find password
+                //
+                // step 3: use email and password to login
             }
         });
     }
--- StepApp/app/src/main/java/com/example/stepapp/RegisterActivity.java
+++ StepApp/app/src/main/java/com/example/stepapp/RegisterActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
@@ -20,6 +21,23 @@ public class RegisterActivity extends AppCompatActivity {
             public void onClick(View view) {
                 startActivity(new Intent(RegisterActivity.this,LoginActivity.class));
 
+            }
+        });
+        // add register functionality
+        Button registerButton = findViewById(R.id.register_btn);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: register new user
+
+                // step 1: find email
+                // step 2: find password
+                // step 3: get confirm password
+                // step 4: check email is valid
+                // step 5: check password is equal to confirm password
+                //
+                // step 6: use email and password to create an account
             }
         });
     }
