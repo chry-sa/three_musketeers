69a4fbb575dee883f89a9cdca4b176073f051df5
nikomyri <nikolaos.myrillas2021@yahoo.com>
2021-11-30 13:36:32 +0100

    first upload

---
+++ StepApp/app/src/main/java/com/example/stepapp/LoginActivity.java
@@ -0,0 +1,25 @@
+package com.example.stepapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_profile);
+        TextView btn=findViewById(R.id.btnToSignUp);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this,RegisterActivity.class));
+
+            }
+        });
+    }
+}
